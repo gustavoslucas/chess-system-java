a16e2ba4d9d3b91d3259f05156e368a52aef731d
Gustavo <ggustavo.lucas@gmail.com>
2024-02-11 19:50:01 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -106,7 +106,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.PlacePiece(p, target);
 		if (capturedPiece != null) {
@@ -117,7 +118,8 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p =(ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.PlacePiece(p, source);
 		if (capturedPiece != null) {
 			board.PlacePiece(capturedPiece, target);
@@ -159,7 +161,8 @@ public class ChessMatch {
 		if (!testCheck(color)) {
 			return false;
 		}
-		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == (color)).collect(Collectors.toList());
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == (color))
+				.collect(Collectors.toList());
 		for (Piece p : list) {
 			boolean[][] mat = p.possibleMoves();
 			for (int i = 0; i < board.getRows(); i++) {
